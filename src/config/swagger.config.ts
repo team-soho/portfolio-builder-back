9bc16bf0d16406013fec773d72cd15ff5d8f799e
ruccess <ruccess0.0@gmail.com>
2024-02-02 14:59:41 +0900

    fix: Add CDN and static-serve library for Swagger bundle files - Not working on Vercel environment

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -29,5 +29,15 @@ export default async (app) => {
         return a.split('.')[0] - b.split('.')[0];
       },
     },
+    customSiteTitle: 'portfolio builder api',
+    customfavIcon: '/static/favicon.ico',
+    //customfavIcon: 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.0/favicon-32x32.png',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.2/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.2/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.2/swagger-ui.min.css',
+    ],
   });
 };
