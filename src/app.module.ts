e493d2134f127d18a521b1483aadef107b2221cb
ruccess <ruccess0.0@gmail.com>
2024-02-04 19:38:53 +0900

    feat: custom configuration, set global module, expandVariables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/static',
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      load: [configuration],
     }),
   ],
   controllers: [AppController],
